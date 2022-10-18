9c3ee86a8668f96ce891d7d4b41161c6b37c6a6c
marcodldev <marcodelisi2@gmail.com>
2022-10-18 15:26:49 +0200

    + main script done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,10 @@
 
 
-let username = " ";
+let username = prompt("What is your name?");
 
-let usersurname = " ";
+let usersurname = prompt("What is your surname?");
 
-let userfavcolor = " ";
+let userfavcolor = prompt("What's your favourite color?");
+
+ document.getElementById("result").innerHTML= `la tua password insicurissima è:${username}${usersurname}${userfavcolor}21`;
 
